c0422c9c7b6c5c320988fcdc9506b1871c8a42f8
RobocikD <robocikd@wp.pl>
2020-11-23 13:30:53 +0100

    Initial commit

---
+++ src/main/java/pl/robocikd/Jdbc.java
@@ -0,0 +1,27 @@
+package pl.robocikd;
+
+import java.sql.*;
+
+public class Jdbc {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/hr?serverTimezone=UTC";
+    public static final String USER = "damian";
+    public static final String PASSWORD = "damian";
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+
+                System.out.println(id + ": " + name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/pl/robocikd/MainDataSource.java
@@ -0,0 +1,46 @@
+package pl.robocikd;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainDataSource {
+
+    public static final String DATABASE_NAME = "hr";
+    public static final String LOCALHOST = "localhost";
+    public static final String USER = "damian";
+    public static final String PASSWORD = "damian";
+
+    public static void main(String[] args) {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName(DATABASE_NAME);
+        dataSource.setServerName(LOCALHOST);
+        dataSource.setUser(USER);
+        dataSource.setPassword(PASSWORD);
+        try {
+            dataSource.setServerTimezone("UTC");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+
+            while (resultSet.next()){
+                String name = resultSet.getString(2);
+                String lastName = resultSet.getString(3);
+
+                System.out.println(name + " " + lastName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
